c357efcf11d64998d2a20f6ea9551857ff978371
Anton Bernatskiy (HPCC) <bernatsk@msu.edu>
2020-02-27 18:43:49 -0500

    selectgpu memory printing functionality now usable
    Prefix of the message about memory is now supplied to printMemUsage
    via a keyword argument, eliminating the need for endless prints in
    the debug messages.
    
    	modified:   selectgpu.py

--- selectgpu.py
+++ selectgpu.py
@@ -19,13 +19,13 @@ def getMemUsage(absolute=False):
 	allgpus = GPUtil.getGPUs()
 	selectedgpus = [ allgpus[id] for id in currentGPUs ]
 	if absolute:
-		return [ gpu.memoryUtil for gpu in selectedgpus ]
-	else:
 		return [ gpu.memoryUsed for gpu in selectedgpus ]
+	else:
+		return [ gpu.memoryUtil for gpu in selectedgpus ]
 
-def printMemUsage(absolute=True):
+def printMemUsage(absolute=True, prefix='Current memory usage: '):
 	if absolute:
 		ustrs = [ f'gpu{gpuid}:{memu:.0f}MiB' for gpuid, memu in zip(currentGPUs, getMemUsage(absolute=True)) ]
 	else:
 		ustrs = [ f'gpu{gpuid}:{memu:.4f}' for gpuid, memu in zip(currentGPUs, getMemUsage(absolute=False)) ]
-	print(f'Current memory usage: ' + ' '.join(ustrs))
+	print(prefix + ' '.join(ustrs))
